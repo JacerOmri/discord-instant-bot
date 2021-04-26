286645ef5b4e6dc24ecee1fec37ef5f1bfa56e2a
Jacer Omri <jaceromri@gmail.com>
2021-04-26 23:25:46 +0100

    search by url if valid

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js"),
 got = require('got'),
+cheerio = require('cheerio'),
 client = new Discord.Client(),
 settings = {
-    prefix: process.env.DISCORD_PREFIX || "-",
+    prefix: process.env.DISCORD_PREFIX || "+",
     token: process.env.DISCORD_TOKEN
 };
 
@@ -10,16 +11,36 @@ client.on("ready", () => {
     console.log("I'm ready !");
 });
 
-const myInstantsExtractor = async (query) => {
+const myInstantsApiExtractor = async (query) => {
     const searchUrl = 'https://www.myinstants.com/api/v1/instants/?format=json&name=' + query
     const body = await got(searchUrl).json()
     const result = body.results[0].sound
     return result;
 }
+
+const isValidMyInstantsUrl = (string) => {
+    let url;
+    
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;  
+    }
+  
+    return  ['www.myinstants.com', 'myinstants.com'].includes(url.host)
+}
+
+const myInstantsUrlExtractor = async (url) => {
+    const response = await got(url)
+    const $ = cheerio.load(response.body);
+    const result = 'https://www.myinstants.com' + $('#instant-page-button[onmousedown]')[0].attribs.onmousedown.match(/play\('(.*)'\)/)[1]
+    return result;
+}
   
 client.on("message", async (message) => {
 
-    console.log({message})
+    if(!message.content.startsWith(settings.prefix)) return
+
     const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
@@ -27,17 +48,22 @@ client.on("message", async (message) => {
         if (!message.member.voice.channel) return message.reply("You must be in a voice channel.");
         if (message.guild.me.voice.channel) return message.reply("I'm already playing.");
 
-        const VoiceConnection = await message.member.voice.channel.join()
+        let VoiceConnection
 
         try {
-            const instant = await myInstantsExtractor(args.join(' '))
+            console.log({args})
+            
+            const instant = isValidMyInstantsUrl(args[0]) ?
+                await myInstantsUrlExtractor(args[0]) :
+                await myInstantsApiExtractor(args.join(' ')) 
 
             // Joining the channel and creating a VoiceConnection.
+            VoiceConnection = await message.member.voice.channel.join()
             VoiceConnection.play(instant).on("finish", () => VoiceConnection.disconnect());
             message.reply("Playing...");
         } catch (error) {
             message.reply("Couldn't play...");
-            VoiceConnection.disconnect()
+            VoiceConnection && VoiceConnection.disconnect()
             console.error(error)
         }
     }
